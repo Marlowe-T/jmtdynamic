66b0c2e8fcedbb18a742c9c2da48dc24c3487a17
Jeff Taylor <j.marlowe.taylor@gmail.com>
2019-04-17 14:01:35 -0400

    fine tune dropmenus

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -1,8 +1,13 @@
 $(function() {
     $("li.loginDropdown > a").on("click", () =>{
         $(".loginDropMenu").toggle("display");
-    });
-    $("li.regDropdown > a").on("click", () =>{
-        $(".regDropMenu").toggle("display");
+        $("li.regDropdown > a").on("click", () =>{
+            $(".loginDropMenu").hide(400);
+            $(".regDropMenu").show(400);
+            $("li.loginDropdown > a").on("click", () =>{
+                $(".regDropMenu").hide(400);
+                $(".loginDropMenu").hide();
+            });   
+        });
     });
 });

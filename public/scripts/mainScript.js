a1b4bc9a986690041a656be948c38a91fe1f5109
Jeff Taylor <j.marlowe.taylor@gmail.com>
2019-04-17 14:50:59 -0400

    add more cool js to navbar

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -1,13 +1,22 @@
 $(function() {
+
+    $("ul.navbar-nav > li").hover( 
+        function(){
+        $(this).addClass('hoverStyle');
+    }, function(){
+        $(this).removeClass('hoverStyle');
+    });
+
     $("li.loginDropdown > a").on("click", () =>{
-        $(".loginDropMenu").toggle("display");
-        $("li.regDropdown > a").on("click", () =>{
-            $(".loginDropMenu").hide(400);
-            $(".regDropMenu").show(400);
-            $("li.loginDropdown > a").on("click", () =>{
-                $(".regDropMenu").hide(400);
-                $(".loginDropMenu").hide();
-            });   
+            $(".loginDropMenu").toggle("display");
+        });
+
+    $("li.regDropdown > a").on("click", () =>{
+        $(".loginDropMenu").hide(400);
+        $(".regDropMenu").show(400);
+        $("li.loginDropdown > a").on("click", () =>{
+            $(".regDropMenu").hide(400);
         });
     });
+
 });

ceded3f8af3e287ef57054ee4c042dfca0235f1c
Jeff Taylor <j.marlowe.taylor@gmail.com>
2019-07-18 20:19:00 -0400

    add index card content and animations

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -1,1 +1,23 @@
 
+$(document).on("scroll", () => {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height() / 2;
+    var leftTags = $("div.fadeLeft");
+    var rightTags = $("div.fadeRight");
+
+    for (var i = 0; i < leftTags.length; i++) {
+        var leftTag = leftTags[i];
+
+        if ($(leftTag).position().top < pageBottom) {
+            $(leftTag).addClass("animate");
+        }
+    }
+
+    for (var i = 0; i < rightTags.length; i++) {
+        var rightTag = rightTags[i];
+
+        if ($(rightTag).position().top < pageBottom) {
+            $(rightTag).addClass("animate");
+        }
+    }
+});
